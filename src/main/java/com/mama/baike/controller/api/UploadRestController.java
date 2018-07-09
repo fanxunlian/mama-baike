d5ccda2b9fa93ef256a87ee9a838db12c62cc032
fanxunlian <fanxunlian@126.com>
2018-07-09 13:45:54 +0800

    图片预览

--- src/main/java/com/mama/baike/controller/api/UploadRestController.java
+++ src/main/java/com/mama/baike/controller/api/UploadRestController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.mama.baike.annotation.AuthIgnore;
 import com.mama.baike.common.ResultBody;
 import com.mama.baike.service.UploadService;
+import com.mongodb.gridfs.GridFSDBFile;
 import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.UUID;
 
 @RestController
@@ -43,6 +47,31 @@ public class UploadRestController {
         File uploadFile = File.createTempFile(UUID.randomUUID().toString(),fileTxt);
         multipartFile.transferTo(uploadFile);
         uploadService.SaveFile("file",uploadFile,filename,filename);
+        return resultBody;
+    }
+
+    @AuthIgnore
+    @RequestMapping("/img/index")
+    public ResultBody getImg(HttpServletRequest request , HttpServletResponse response) throws FileUploadException, IOException
+    {
+        String imgeName = request.getParameter("imgeName");
+        ResultBody resultBody = new ResultBody();
+        GridFSDBFile gridFSDBFile= uploadService.retrieveFileOne("file",imgeName);
+
+        if (gridFSDBFile != null) {
+            OutputStream sos = response.getOutputStream();
+            response.setContentType("application/octet-stream");
+            // 获取原文件名
+            String name = (String) gridFSDBFile.get("filename");
+            String fileName = new String(name.getBytes("GBK"), "ISO8859-1");
+            // 设置下载文件名
+            response.addHeader("Content-Disposition", "attachment; filename=\""	+ fileName + "\"");
+            // 向客户端输出文件
+            gridFSDBFile.writeTo(sos);
+            sos.flush();
+            sos.close();
+        }
+
         return resultBody;
     }
 
