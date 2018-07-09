26872deda0519348d76a44e5e1f5d0e81e1ef054
fanxunlian <fanxunlian@126.com>
2018-07-09 11:41:56 +0800

    上传文件

--- src/main/java/com/mama/baike/controller/api/UploadRestController.java
+++ src/main/java/com/mama/baike/controller/api/UploadRestController.java
@@ -1,20 +1,19 @@
 package com.mama.baike.controller.api;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.mama.baike.annotation.AuthIgnore;
 import com.mama.baike.common.ResultBody;
-import com.mama.baike.service.MongoService;
 import com.mama.baike.service.UploadService;
 import org.apache.tomcat.util.http.fileupload.FileUploadException;
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/upload")
@@ -24,28 +23,26 @@ public class UploadRestController {
     private UploadService uploadService;
     /**
      * 上传图片
-     * @param file
      * @return
      */
     @AuthIgnore
     @RequestMapping("/img")
-    public ResultBody uploadImg(@RequestParam("file") MultipartFile file) throws FileUploadException, IOException
+    public ResultBody uploadImg(@RequestParam("file") MultipartFile multipartFile) throws FileUploadException, IOException
     {
         ResultBody resultBody = new ResultBody();
-        String filename = file.getOriginalFilename();
+        String filename = multipartFile.getOriginalFilename();
         String fileTxt = filename.substring(filename.lastIndexOf(".")+1);
-        byte[] filebuf = file.getBytes();
+        byte[] filebuf = multipartFile.getBytes();
         if(filebuf == null || filebuf.length == 0 )
         {
             resultBody.setCode(500);
             resultBody.setMessage("文件为空");
             return resultBody;
         }
-        File uploadFile = null;
 
+        File uploadFile = File.createTempFile(UUID.randomUUID().toString(),fileTxt);
+        multipartFile.transferTo(uploadFile);
         uploadService.SaveFile("file",uploadFile,filename,filename);
-
-
         return resultBody;
     }
 
